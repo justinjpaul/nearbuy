436765617ecf18e5ca3671fec7f622695c30b32f
Justin Paul <justpaul@umich.edu>
2023-02-18 12:40:59 -0800

    change map rendering listing

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import {renderToString} from 'react-dom/server'
 import mapboxgl from 'mapbox-gl';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
 
@@ -22,31 +26,49 @@ const StanfordMap = () => {
                     location: [-122.1679, 37.4276],
                     product: 'Latte',
                     price: '$4.50',
-                    image: 'https://example.com/latte.jpg'
+                    image: 'https://example.com/latte.jpg',
+                    email: 'abc@payme.com'
                 },
                 {
                     name: 'Hobee\'s',
                     location: [-122.1824, 37.4454],
                     product: 'Pancakes',
                     price: '$9.99',
-                    image: 'https://example.com/pancakes.jpg'
+                    image: 'https://example.com/pancakes.jpg',
+                    email: 'thing@yo.com'
                 },
                 // Add more local businesses here
             ];
 
+
+            function makeListing(business) {
+                return (
+                  <div>
+                    <h3>{business.name}</h3>
+                    <img src={business.image} alt={business.product} style={{ width: '100%', maxWidth: '300px', border: 'none' }} />
+                    <p>
+                      <strong>Price:</strong> {business.price}
+                    </p>
+                    <p>
+                        <strong>Contact:</strong>
+                        <a href={`mailto:${business.email}`}>
+                            <FontAwesomeIcon icon={faEnvelope} />
+                        </a>
+                    </p>
+                    <br></br>
+                    <button>Buy now</button>
+                  </div>
+                );
+              }
+              
+
             localBusinesses.forEach((business) => {
-                const popupHtml = `
-          <div>
-            <h3>${business.name}</h3>
-            <img src="${business.image}" alt="${business.product}" style="width: 100%; max-width: 300px;">
-            <p><strong>Price:</strong> ${business.price}</p>
-            <button>Buy now</button>
-          </div>
-        `;
+                const popupHtml = makeListing(business)
+                console.log(renderToString(popupHtml))
 
                 new mapboxgl.Marker()
                     .setLngLat(business.location)
-                    .setPopup(new mapboxgl.Popup().setHTML(popupHtml))
+                    .setPopup(new mapboxgl.Popup().setHTML(renderToString(popupHtml)))
                     .addTo(map);
             });
 
